95fedd9923e4854a240f3cfe6b0ba66dba44148a
Max Hausch <max.hausch@helsinki-systems.de>
2024-05-14 12:28:41 +0200

    feat: Update code to work with 3cx V20

--- main.py
+++ main.py
@@ -23,6 +23,42 @@ VERIFY_SSL = True
 PBX_URL = environ.get("PBX_URL", "my3cxinstallation.my3cx.de")
 
 
+def do_login(username: str, password: str) -> (str, str, dict, str):
+    # Get login cookie
+    request = requests.post(
+        f"https://{PBX_URL}/webclient/api/Login/GetAccessToken",
+        verify=VERIFY_SSL,
+        json={"Password": password, "Username": username},
+    )
+    if not request.status_code == 200:
+        raise ValueError(f"Failed to login. {request.status_code}")
+    cookies = request.cookies
+    # connect token
+    request = requests.post(
+        f"https://{PBX_URL}/connect/token",
+        cookies=request.cookies,
+        verify=VERIFY_SSL,
+        data={"client_id": "Webclient", "grant_type": "refresh_token"},
+    )
+    bearer_token = request.json()["access_token"]
+
+    if not request.status_code == 200:
+        raise ValueError(f"Failed to login. {request.status_code}")
+    request = requests.post(
+        f"https://{PBX_URL}/webclient/api/MyPhone/session",
+        headers={"Authorization": f"Bearer {bearer_token}"},
+        cookies=cookies,
+        verify=VERIFY_SSL,
+        json={"name": "Webclient", "version": "nope", "isHuman": True},
+    )
+    if not request.status_code == 200:
+        raise ValueError(f"Failed to get phone session {request.status_code}")
+    session_key = request.json()["sessionKey"]
+    pass_val = request.json()["pass"]
+
+    return session_key, bearer_token, cookies, pass_val
+
+
 def get_phone_config(ws_pass: str, current_session_key: str, cookies: RequestsCookieJar) -> str:
     """
     This function opens a websocket to the 3cx server and queries the current
@@ -91,52 +127,19 @@ def get_phone_config(ws_pass: str, current_session_key: str, cookies: RequestsCo
 #########################################################################
 
 
-# Get login cookie
-request = requests.post(
-    f"https://{PBX_URL}/webclient/api/Login", verify=VERIFY_SSL, json={"Password": PASSWORD, "Username": EXTENSION}
-)
-if not request.status_code == 200:
-    raise ValueError("Failed to login")
-
 # Get phone session id
 # Actually, this json can be empty...
-request = requests.post(
-    f"https://{PBX_URL}/webclient/api/MyPhone/session",
-    cookies=request.cookies,
-    verify=VERIFY_SSL,
-    json={"name": "Webclient", "version": "nope", "isHuman": True},
-)
-if not request.status_code == 200:
-    print(request.status_code)
-    raise ValueError("Failed to get phone session")
-session_key = request.json()["sessionKey"]
+session_key, bearer_token, cookies, pass_val = do_login(EXTENSION, PASSWORD)
 
 # Initiate the connection to query phone configuration
-phone_config = get_phone_config(request.json()["pass"], session_key, request.cookies)
-
+phone_config = get_phone_config(pass_val, session_key, cookies)
 
 #########################################################################
 # Call part
 #########################################################################
 
-
-# Get login cookie
-request = requests.post(
-    f"https://{PBX_URL}/webclient/api/Login", json={"Password": PASSWORD, "Username": EXTENSION}, verify=VERIFY_SSL
-)
-if not request.status_code == 200:
-    raise ValueError("Failed to login")
-
-# Get phone session id
-request = requests.post(
-    f"https://{PBX_URL}/webclient/api/MyPhone/session",
-    json={"name": "Webclient", "version": "nope", "isHuman": True},
-    verify=VERIFY_SSL,
-    cookies=request.cookies,
-)
-if not request.status_code == 200:
-    raise ValueError("Failed to get phone session")
-session_key = request.json()["sessionKey"]
+# No idea why we need to log in a second time
+session_key, bearer_token, cookies, pass_val = do_login(EXTENSION, PASSWORD)
 
 # This second part of the body contains the actual number and phone information
 # The first byte of this sequence must be \n, the second byte is the length of
@@ -153,9 +156,13 @@ body_part_one = b"\x08\x77\xba\x07" + bytes([len(body_part_two)])
 request = requests.post(
     f"https://{PBX_URL}/MyPhone/MPWebService.asmx",
     data=(body_part_one + body_part_two.encode()),
-    headers={"content-type": "application/octet-stream", "myphonesession": session_key},
+    headers={
+        "content-type": "application/octet-stream",
+        "myphonesession": session_key,
+        "Authorization": f"Bearer {bearer_token}",
+    },
     verify=VERIFY_SSL,
-    cookies=request.cookies,
+    cookies=cookies,
 )
 if not request.status_code == 200:
-    raise ValueError("Failed to place call")
+    raise ValueError(f"Failed to call number {request.status_code}")
